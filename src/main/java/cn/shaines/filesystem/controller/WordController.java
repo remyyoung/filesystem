7ed203bda5cab2dd9a7b94913547b923ba9405a2
yangchuan <remyyoung@qq.com>
2019-12-10 17:44:15 +0800

    添加预览展示页面overview 添加word有关请求控制类

---
+++ src/main/java/cn/shaines/filesystem/controller/WordController.java
@@ -0,0 +1,20 @@
+package cn.shaines.filesystem.controller;
+
+import cn.shaines.filesystem.entity.PageOfficeEntry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description: TODO
+ * <p>实现word在线预览可编辑</p>
+ * @author: yc
+ * @date 2019/12/10 17:10
+ */
+@RestController
+public class WordController {
+
+    @Autowired
+    private PageOfficeEntry pageOfficeEntry;
+
+
+}
